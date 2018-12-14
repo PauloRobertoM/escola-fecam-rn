09fcd5ec4ce1d1e2a9f34184455b3bf1656a2ab1
Paulo Alves <front@agencianovi.com.br>
2018-12-14 11:40:47 -0300

    First

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php include 'components/header.php'; ?>
+
+    <section class="vitrine" id="inscricao">
+    	<div class="content">
+    		<div class="container">
+    			<div class="form">
+    				<h2>formulário de pré-inscrição</h2>
+    				<p>Se já possui seu voucher <a href="" class="hvr-underline-from-center">clique aqui</a>, se não preencha sua pré-inscrição abaixo</p>
+    				<form>
+                        <input type="text" name="nome" id="nome" class="form-control" placeholder="Nome" value="" />
+                        <input type="text" name="email" id="email" class="form-control" placeholder="Email" value="" />
+                        <select>
+                        	<option>Município</option>
+                        	<option value="saab">Saab</option>
+                        	<option value="mercedes">Mercedes</option>
+                        	<option value="audi">Audi</option>
+                        </select>
+                        <select>
+                        	<option value="volvo">Volvo</option>
+                        	<option value="saab">Saab</option>
+                        	<option value="mercedes">Mercedes</option>
+                        	<option value="audi">Audi</option>
+                        </select>
+
+                        <button>enviar</button>
+                    </form>
+    			</div><!-- form -->
+    		</div><!-- container -->
+    	</div><!-- content -->
+    	<img src="assets/images/vitrine.jpg" alt="" />
+    </section><!-- vitrine -->
+	
+	<section class="a-escola" id="a-escola">
+	    <div class="container">
+	    	<img src="assets/images/back.jpg" alt="" class="back" />
+
+	    	<h2 class="titulo-geral">A ESCOLA</h2>
+			
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+	    			<p class="p-geral">A ESCOLA DA FECAM foi criada através da Resolução 000/2018, em 00 de janeiro de 2018 pelo Vereador Odair Diniz então Presidente da <span>FECAM/RN</span>, o documento também regulamenta a organização administrativa da ESCOLA.</p>
+					<p class="p-geral">A Escola surge com a finalidade de no futuro integrar-se ao Sistema de Ensino do Legislativo, articulado nacionalmente pela Associação Brasileira de Escolas do Legislativo – ABEL, formado por órgãos do Senado Federal, da Câmara dos Deputados, do Tribunal de Contas da União, de Assembleias Legislativas Estaduais, da Câmara Distrital, de Câmaras Municipais e de Tribunais de Contas Estaduais.</p>
+					<p class="p-geral">Como instrumento de regulamentação, a referida Resolução orienta a organização e o funcionamento da estrutura administrativa da Escola, a operacionalização dos serviços e o ordenamento dos relacionamentos com órgãos da comunidade interna da FEDERAÇÃO e com segmentos da comunidade externa.</p>
+	    		</div><!-- md-6 -->
+	    		<div class="col-md-6 col-sm-6">
+	    			<img src="assets/images/a-escola.jpg" alt="" class="img-responsive" />
+	    		</div><!-- md-6 -->
+	    	</div><!-- row -->
+	    </div><!-- container -->
+	</section><!-- a-escola -->
+
+	<section class="diretoria" id="diretoria">
+	    <div class="container">
+	    	<h2 class="titulo-geral">DIRETORIA</h2>
+			
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+	    			<div class="item item1">
+	    				<img src="assets/images/pessoa1.png" alt="" />
+	    				<h2>Diretor Geral</h2>
+	    				<p>Nome</p>
+	    			</div><!-- item -->
+	    		</div><!-- md-6 -->
+	    		<div class="col-md-6 col-sm-6">
+	    			<div class="item item2">
+	    				<img src="assets/images/pessoa2.png" alt="" />
+	    				<h2>Coordenação Pedagógica e de Projtetos</h2>
+	    				<p>Nome</p>
+	    			</div><!-- item -->
+	    		</div><!-- md-6 -->
+	    	</div><!-- row -->
+	    	<div class="row">
+				<div class="col-md-6 col-sm-6">
+	    			<div class="item item3">
+	    				<img src="assets/images/pessoa3.png" alt="" />
+	    				<h2>Direção Acadêmica, Pedagógica e de projetos</h2>
+	    				<p>Nome</p>
+	    			</div><!-- item -->
+	    		</div><!-- md-6 -->
+	    		<div class="col-md-6 col-sm-6">
+	    			<div class="item item4">
+	    				<img src="assets/images/pessoa4.png" alt="" />
+	    				<h2>Conselho Geral</h2>
+	    				<p>Nome</p>
+	    			</div><!-- item -->
+	    		</div><!-- md-6 -->
+	    	</div><!-- row -->
+	    </div><!-- container -->
+	</section><!-- diretoria -->
+
+	<section class="contato" id="contato">
+		<div class="container">
+			<div class="row">
+				<div class="item-cursos-mobile">
+					<div class="col-md-6 col-sm-6">
+						<div class="item-cursos">
+							<h2 class="titulo-geral">CURSOS</h2>
+							<p class="p-geral">Os cursos oferecem aos Vereadores, Servidores e a Comunidade em geral oportunidades de avanço em sua formação e complementação, priorizando à demanda interna.</p>
+							<p class="p-geral">Esses cursos se destinam à capacitação, à atualização e ao treinamento de Edis, Funcionários e Comunidade em geral, quer por iniciativa da <span>FECAM/RN</span>, quer por solicitação das <span>CÂMARAS MUNICIPAIS</span>, quando não direcionados especificamente a um público explicito, esses cursos disponibilizam-se vagas para a comunidade em geral.</p>
+							<p class="p-geral">1º - Faça sua Pré-inscrição.<br />2º - Vá até a Câmara Municipal de sua cidade e solicite seu <span>VOUCHER</span>.<br />3º - Com o <span>VOUCHER</span> em mãos valide sua inscrição.<br />4º - Acesse a <span>ÁREA DO ALUNO</span>.</p>
+
+							<a href="" class="inscreva-se">INSCREVA-SE</a>
+							<p class="inscricao">SE VOCÊ JÁ FEZ A INSCRIÇÃO, <a href="#inscricao" class="hvr-underline-from-center scroll">CLIQUE AQUI.</a></p>
+						</div><!-- item -->
+					</div><!-- md-6 -->
+				</div><!-- item-cursos-mobile -->
+				<div class="item-contato-mobile">
+					<div class="col-md-6 col-sm-6">
+						<div class="item-contato">
+							<h2 class="titulo-geral">CONTATO</h2>
+
+							<form>
+		                        <input type="text" name="nome" id="nome" class="form-control" placeholder="NOME" value="" />
+		                        <input type="text" name="email" id="email" class="form-control" placeholder="E-MAIL" value="" />
+		                        <textarea name="mensagem" id="mensagem" class="form-control" placeholder="MENSAGEM"></textarea>
+
+		                        <button>enviar</button>
+		                    </form>
+		                    <p><i class="fa fa-phone" aria-hidden="true"></i> Tel.: (84) 3211-0845</p>
+		                    <p><i class="fa fa-mobile" aria-hidden="true"></i> Cel.: (84) 9.9666-6768</p>
+		                    <p><i class="fa fa-envelope" aria-hidden="true"></i> E-mail: fecam.rn.adm@hotmail.com</p>
+						</div><!-- item -->
+					</div><!-- md-6 -->
+				</div><!-- item-contato-mobile -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- contato -->
+
+<?php include 'components/footer.php'; ?>
